5b8515132747ef8d4990af75587fa45ed71855e9
Robin <robinlaurentius@gmail.com>
2024-05-02 18:37:12 +0200

    Remove dist folder and update .gitignore

---
+++ dist/assets/index-De-JUd8g.js
@@ -0,0 +1,13 @@
+(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const o of document.querySelectorAll('link[rel="modulepreload"]'))r(o);new MutationObserver(o=>{for(const i of o)if(i.type==="childList")for(const e of i.addedNodes)e.tagName==="LINK"&&e.rel==="modulepreload"&&r(e)}).observe(document,{childList:!0,subtree:!0});function c(o){const i={};return o.integrity&&(i.integrity=o.integrity),o.referrerPolicy&&(i.referrerPolicy=o.referrerPolicy),o.crossOrigin==="use-credentials"?i.credentials="include":o.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function r(o){if(o.ep)return;o.ep=!0;const i=c(o);fetch(o.href,i)}})();(function(){function a(){for(var e=document.getElementsByTagName("script"),t=0;t<e.length;t++)if(e[t].getAttribute("websiteId"))return{websiteId:e[t].getAttribute("websiteId")};return{websiteId:null}}const s=a().websiteId;console.log("Gebruiker geladen:",s);function c(){let e=document.getElementById("notification-area");return e||(e=document.createElement("div"),e.id="notification-area",document.body.appendChild(e)),e}function r(e){const t=e.target.closest(".notification");t&&(t.style.transform="translateX(100%)",setTimeout(()=>t.remove(),500),e.stopPropagation())}function o(e,t,f,u,m,l){const p=c(),n=document.createElement("div");n.className="notification",n.style.transform="translateX(120%)",n.style.cursor="pointer",n.innerHTML=`
+      ${e?`<img src="${e}">`:""}
+      <div class="close--icon">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="15" height="15">
+          <path d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z"></path>
+        </svg>
+      </div>
+      <div class="content-wrapper">
+        <div class="sender">${t}</div>
+        <hr/>
+        <div class="message">${f}</div>
+      </div>
+    `,n.addEventListener("click",function(g){g.target.closest(".close--icon")||(window.location.href=u)});const d=n.querySelector(".close--icon");d&&d.addEventListener("click",r),setTimeout(()=>{n.style.transform="translateX(0)"},l),p.appendChild(n),setTimeout(()=>{r({target:n})},m+l)}const i=document.createElement("script");i.src="https://cdn.socket.io/4.0.0/socket.io.min.js",i.onload=function(){const e=io("http://localhost:8080");e.on("notification",function(t){console.log("Notificatie ontvangen:",t),o(t.imgUrl,t.sender,t.message,t.link,t.showTimeInMs,t.delayInMs)}),e.on("connect",()=>{e.emit("userLocation",{websiteId:s,location:window.location.pathname})})},document.head.appendChild(i)})();
